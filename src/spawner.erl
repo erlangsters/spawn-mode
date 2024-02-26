ac2339781f5ffa752a40bf6fe6a8c04f9ffe7303
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2024-02-27 01:19:24 +0800

    Add LICENSE.txt and source file headers

--- src/spawner.erl
+++ src/spawner.erl
@@ -1,3 +1,12 @@
+%%
+%% Copyright (c) 2024, Byteplug LLC.
+%%
+%% This source file is part of a project made by the Erlangsters community and
+%% is released under the MIT license. Please refer to the LICENSE.txt file that
+%% can be found at the root of the project directory.
+%%
+%% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, February 2024
+%%
 -module(spawner).
 
 -export_type([mode/0]).
