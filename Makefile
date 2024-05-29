erlangsters/process-spawner

Update project description

Makefile
@@ -1,5 +1,5 @@
 PROJECT = process-spawner
-PROJECT_DESCRIPTION = New project
+PROJECT_DESCRIPTION = An Erlang process spawner API that solves a design fundamental issue.
 PROJECT_VERSION = 0.1.0
 
 include erlang.mk

erlangsters/spawn-mode

Makefile
@@ -1,5 +1,5 @@
 PROJECT = process-spawner
-PROJECT_DESCRIPTION = New project
+PROJECT_DESCRIPTION = An Erlang process spawner API that solves a fundamental design issue.
 PROJECT_VERSION = 0.1.0
 
 include erlang.mk