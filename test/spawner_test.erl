214567a7ec47bc18289a98b143b2d7db317b7658
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2024-05-31 18:07:21 +0800

    Increase code coverage of unit test
    It tests now all spawn mode variants.

--- test/spawner_test.erl
+++ test/spawner_test.erl
@@ -12,6 +12,13 @@
 
 % XXX: It does not test all the spawn variants.
 
+-define(SPAWN_MODE1, no_link).
+-define(SPAWN_MODE2, link).
+-define(SPAWN_MODE3, monitor).
+-define(SPAWN_MODE4, {monitor, [{tag, 'OFF'}]}).
+-define(SPAWN_MODE5, {no_link, [{priority, low}]}).
+-define(SPAWN_MODE6, {{monitor, [{tag, 'OFF'}]}, [{priority, low}]}).
+
 is_linked(Pid) ->
     case process_info(Pid, links) of
         {links, Links} ->
@@ -27,21 +34,21 @@ spawner_test() ->
         Root ! '$test_ok'
     end,
 
-    Pid1 = spawner:spawn(no_link, Fun),
+    Pid1 = spawner:spawn(?SPAWN_MODE1, Fun),
     false = is_linked(Pid1),
     ok = receive
         '$test_ok' ->
             ok
     end,
 
-    Pid2 = spawner:spawn(link, Fun),
+    Pid2 = spawner:spawn(?SPAWN_MODE2, Fun),
     true = is_linked(Pid2),
     ok = receive
         '$test_ok' ->
             ok
     end,
 
-    {Pid3, Monitor3} = spawner:spawn(monitor, Fun),
+    {Pid3, Monitor3} = spawner:spawn(?SPAWN_MODE3, Fun),
     false = is_linked(Pid3),
     ok = receive
         '$test_ok' ->
@@ -49,6 +56,37 @@ spawner_test() ->
     end,
     ok = receive
         {'DOWN', Monitor3, process, Pid3, normal} ->
+            ok
+    end,
+
+    {Pid4, Monitor4} = spawner:spawn(?SPAWN_MODE4, Fun),
+    false = is_linked(Pid4),
+    ok = receive
+        '$test_ok' ->
+            ok
+    end,
+    ok = receive
+        {'OFF', Monitor4, process, Pid4, normal} ->
+            ok
+    end,
+
+    Pid5 = spawner:spawn(?SPAWN_MODE5, Fun),
+    false = is_linked(Pid5),
+    {priority, low} = process_info(Pid5, priority),
+    ok = receive
+        '$test_ok' ->
+            ok
+    end,
+
+    {Pid6, Monitor6} = spawner:spawn(?SPAWN_MODE6, Fun),
+    false = is_linked(Pid6),
+    {priority, low} = process_info(Pid6, priority),
+    ok = receive
+        '$test_ok' ->
+            ok
+    end,
+    ok = receive
+        {'OFF', Monitor6, process, Pid6, normal} ->
             ok
     end,
 
