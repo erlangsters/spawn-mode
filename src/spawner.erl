6fe0af712efe48d33eae19690e68a1fcfb81c7aa
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2024-06-03 21:33:09 +0800

    Fix monitor/process options ambiguity
    Consider the `{monitor, Options}` spawn mode. It could refer to either `{{monitor, []}, Options}` or `{{monitor, Options}, []}`.

--- src/spawner.erl
+++ src/spawner.erl
@@ -35,42 +35,119 @@
 
 -spec spawn(mode(), function()) -> spawn_return().
 spawn(Mode, Fun) ->
-    spawn_opt(Fun, mode_to_options(Mode)).
+    {Link, Options} = normalize_mode(Mode),
+    spawn_opt(Fun, to_spawn_options(Link, Options)).
 
 -spec spawn(mode(), node(), function()) -> spawn_return().
 spawn(Mode, Node, Fun) ->
-    spawn_opt(Node, Fun, mode_to_options(Mode)).
+    {Link, Options} = normalize_mode(Mode),
+    spawn_opt(Node, Fun, to_spawn_options(Link, Options)).
 
 -spec spawn(mode(), module(), function(), [term()]) -> spawn_return().
 spawn(Mode, Module, Function, Args) ->
-    spawn_opt(Module, Function, Args, mode_to_options(Mode)).
+    {Link, Options} = normalize_mode(Mode),
+    spawn_opt(Module, Function, Args, to_spawn_options(Link, Options)).
 
 -spec spawn(mode(), node(), module(), function(), [term()]) -> spawn_return().
 spawn(Mode, Node, Module, Function, Args) ->
-    spawn_opt(Node, Module, Function, Args, mode_to_options(Mode)).
+    {Link, Options} = normalize_mode(Mode),
+    spawn_opt(Node, Module, Function, Args, to_spawn_options(Link, Options)).
 
 -spec setup(pid(), mode()) -> spawn_return().
 setup(_Pid, _Mode) ->
     % XXX: To be implemented.
     ok.
 
-%%
-%% Convert our spawn mode to the options suitable for the `spawn_request/x`
-%% functions.
-%%
--spec mode_to_options(mode()) -> ok.
-mode_to_options(Link) when is_atom(Link) ->
-    mode_to_options({Link, []});
-mode_to_options({monitor, Options}) ->
-    mode_to_options({{monitor, Options}, []});
-mode_to_options({Link, Options}) ->
+-spec is_process_option(erlang:monitor_option() | option()) -> boolean().
+is_process_option({Name, _}) ->
+    lists:member(Name, [
+        priority, fullsweep_after, min_heap_size, min_bin_vheap_size,
+        max_heap_size, message_queue_data, async_dist
+    ]).
+
+-spec normalize_mode(mode()) -> {link(), [option()]}.
+normalize_mode(Mode) ->
+    case Mode of
+        {monitor, []} ->
+            {{monitor, []}, []};
+        {monitor, Options} ->
+            % We need to resolve an ambiguity: options could be either the
+            % monitor options of the process options. We look at the first
+            % option to determine which one it is.
+            IsProcessOption = is_process_option(hd(Options)),
+            % lists:foreach(fun(Option) ->
+            %     case is_process_option(Option) of
+            %         IsProcessOption ->
+            %             ok;
+            %         _ ->
+            %             erlang:error(badarg)
+            %     end
+            % end, tl(Options)),
+            case IsProcessOption of
+                true ->
+                    {{monitor, []}, Options};
+                false ->
+                    {{monitor, Options}, []}
+            end;
+        {Link, Options} ->
+            {Link, Options};
+        Link ->
+            {Link, []}
+    end.
+
+-spec to_spawn_options(link(), [option()]) -> erlang:spawn_opt_option().
+to_spawn_options(Link, Options) ->
     case Link of
         no_link ->
             [];
         link ->
             [link];
         monitor ->
-            [monitor];
+            [{monitor, []}];
         {monitor, MonitorOptions} ->
             [{monitor, MonitorOptions}]
     end ++ Options.
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+is_process_option_test() ->
+    ?assertEqual(false, is_process_option({tag, 'OFF'})),
+    ?assertEqual(true, is_process_option({priority, low})),
+
+    ok.
+
+normalize_mode_test() ->
+    ?assertEqual({no_link, []}, normalize_mode(no_link)),
+    ?assertEqual({no_link, [{priority, low}]}, normalize_mode({no_link, [{priority, low}]})),
+
+    ?assertEqual({link, []}, normalize_mode(link)),
+    ?assertEqual({link, [{priority, low}]}, normalize_mode({link, [{priority, low}]})),
+
+    ?assertEqual({monitor, []}, normalize_mode(monitor)),
+    ?assertEqual({{monitor, []}, [{priority, low}]}, normalize_mode({monitor, [{priority, low}]})),
+    ?assertEqual({{monitor, [{tag, 'OFF'}]}, []}, normalize_mode({monitor, [{tag, 'OFF'}]})),
+    ?assertEqual({{monitor, [{tag, 'OFF'}]}, [{priority, low}]}, normalize_mode({{monitor, [{tag, 'OFF'}]}, [{priority, low}]})),
+
+    % Mixing monitor and process options (we disabled this feature).
+    % ?assertException(error, badarg, normalize_mode({monitor, [{tag, 'OFF'}, {priority, low}]})),
+    % ?assertException(error, badarg, normalize_mode({monitor, [{priority, low}, {tag, 'OFF'}]})),
+    % ?assertException(error, badarg, normalize_mode({{monitor, [{priority, low}]}, [{tag, 'OFF'}]})),
+
+    ok.
+
+to_spawn_options_test() ->
+    ?assertEqual([], to_spawn_options(no_link, [])),
+    ?assertEqual([{priority, low}], to_spawn_options(no_link, [{priority, low}])),
+
+    ?assertEqual([link], to_spawn_options(link, [])),
+    ?assertEqual([link, {priority, low}], to_spawn_options(link, [{priority, low}])),
+
+    ?assertEqual([{monitor, []}], to_spawn_options(monitor, [])),
+    ?assertEqual([{monitor, [{tag, 'OFF'}]}], to_spawn_options({monitor, [{tag, 'OFF'}]}, [])),
+    ?assertEqual([{monitor, []}, {priority, low}], to_spawn_options(monitor, [{priority, low}])),
+
+    ok.
+
+-endif.
