de657bd660890cf359388cd4a71e01cb26d62de6
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-06-21 02:00:42 +0800

    Rename LICENSE.txt to LICENSE.md

--- test/spawner_test.erl
+++ test/spawner_test.erl
@@ -2,7 +2,7 @@
 %% Copyright (c) 2025, Byteplug LLC.
 %%
 %% This source file is part of a project made by the Erlangsters community and
-%% is released under the MIT license. Please refer to the LICENSE.txt file that
+%% is released under the MIT license. Please refer to the LICENSE.md file that
 %% can be found at the root of the project directory.
 %%
 %% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, February 2024
